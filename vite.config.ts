7c2d5b625cd820f8213ff373aeae0d44a9bc0487
Peyman <itispey@gmail.com>
2026-09-18 13:04:43 +0330

    feat: add deployment script and Cloudflare integration
    - Updated package.json to include a deploy script using Vite and Wrangler.
    - Added @cloudflare/vite-plugin to Vite configuration for Cloudflare support.
    - Created wrangler.jsonc for Cloudflare configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import path from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { cloudflare } from "@cloudflare/vite-plugin";
 import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), basicSsl()],
+  plugins: [react(), tailwindcss(), cloudflare(), basicSsl()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -16,7 +17,7 @@ export default defineConfig({
     // Allows testing via Telegram's HTTPS-only WebView tunnels (e.g. ngrok/cloudflared)
     host: true,
     // Default 5173 falls in Windows Hyper-V reserved range 5146–5245 (EACCES)
-    port: 3002,
+    // port: 3002,
     allowedHosts: true, // TODO: Remove this in production
   },
 })
